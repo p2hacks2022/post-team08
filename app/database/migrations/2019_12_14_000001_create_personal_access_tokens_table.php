92eb011df75f45a037036eda9b9bf16b62dbc700
nagashimakouta <106372505+nagashimakouta@users.noreply.github.com>
2022-12-15 19:42:53 +0900

    Update 2019_12_14_000001_create_personal_access_tokens_table.php

--- app/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ app/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,13 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('diary_contents', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('diary_id');->unique();
+            $table->string('diary_txt', 100);
+            $table->bigInteger('star');
+            $table->tinyInteger('diary_flag');
+            $table->date('diary_time');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('diary_contents');
     }
 }
